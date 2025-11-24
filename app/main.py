ae501da141c46f050271be5af3574d552f2fa74e
Navisper <alobuenastarde@gmail.com>
2025-11-23 22:58:03 -0500

    Updated main.py to allow all origins

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Gratus - Sistema de Gestión de Ventas")
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
